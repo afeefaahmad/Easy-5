b6d583351de3aa485efc864b551cadafb6d6571a
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2024-09-11 16:50:51 +0530

    Create Kth element swap.cpp

---
+++ Kth element swap.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+    int main(){
+        int n,k;
+        cout<<"Enter number of elements ";
+        cin>>n;
+        int a[n];
+        
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        cout<<"Enter value of k ";
+        cin>>k;
+        
+        int temp=a[k-1];
+        a[k-1]=a[n-k];
+        a[n-k]=temp;
+        
+        for(int i=0;i<n;i++){
+            cout<<a[i];
+        }
+        
+    
+}
