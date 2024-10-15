cdb48f643c8eee5f28c6e71df817884aaa203e93
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2024-10-15 06:39:38 +0530

    Update ClimbingStairs.cpp

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main(){
+    
+    int n;
+    cout<<"enter number of steps: ";
+    cin>>n;
+    
+    if(n==1){
+        cout<<"No of ways to climb is 1";
+    }
+    vector<int> cs(n,0);
+    cs[0]=1;
+    cs[1]=2;
+    
+    for(int i=2;i<n;i++){
+        cs[i]=cs[i-1]+cs[i-2];
+    }
+    
+    cout<<"number of ways taken are:";
+    cout<<cs[n-1];
+    
+    
+    return 0;
+}
