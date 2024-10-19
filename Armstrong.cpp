05bb775a93e94f6d8b41e1fae816dd6499eeb2be
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2024-10-19 14:21:09 +0530

    Update Armstrong.cpp

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -21,3 +21,35 @@ int main(){
     }
     return 0;
 }
+
+---------------------------------------------------------------------------
+//Armstrong number for any number of digits.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int rem,curr_num,num,result=0,num_digits;
+    cout<<"Enter number :";
+    cin>>num;
+    curr_num = num;
+    while(curr_num!=0){
+        rem=curr_num%10;
+        curr_num=curr_num/10;
+        num_digits++;
+    }
+    curr_num=num;
+    while(curr_num!=0){
+        rem=curr_num%10;
+        result+= pow(rem,num_digits);
+        curr_num=curr_num/10;
+    }
+    if(result==num){
+        cout<<"Armstrong";
+    }
+    else{
+        cout<<"Not armstrong";
+    }
+    return 0;
+}
